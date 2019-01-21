fc68ba1f26e10a7a11dd2e9022c35d7218dee046
Sam King <samuel-king@live.com.au>
2019-01-21 13:28:03 +1100

    Added function to remove HTML tags from title, made print statement more readable.

--- Web-Title-Scraper.py
+++ Web-Title-Scraper.py
@@ -1,9 +1,18 @@
 import requests
 import bs4
+import re
+
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
 URL = input("Enter the URL here: ")
 
 res = requests.get(URL.strip())
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-title = soup.select('title')
-print(title)
+title_list = soup.select('title')
+
+title = title_list[0]
+
+print("The title of the web page at " + URL + "is: \"" + remove_html_tags(str(title)) + "\"")
+

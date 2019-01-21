be1c0d78295308076c97140facabd0a42368ddd0
Sam King <samuel-king@live.com.au>
2019-01-21 12:51:27 +1100

    Added input method

--- Web-Title-Scraper.py
+++ Web-Title-Scraper.py
@@ -1,10 +1,9 @@
 import requests
 import bs4
 
-res = requests.get('https://eddb.io/')
+URL = input("Enter the URL here: ")
 
+res = requests.get(URL.strip())
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-
 title = soup.select('title')
-
 print(title)

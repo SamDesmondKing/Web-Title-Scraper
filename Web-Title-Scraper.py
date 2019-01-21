82cecf7e8846041eddae215247e61feb782e9f38
Sam King <samuel-king@live.com.au>
2019-01-21 13:34:38 +1100

    Added comments

--- Web-Title-Scraper.py
+++ Web-Title-Scraper.py
@@ -2,17 +2,22 @@ import requests
 import bs4
 import re
 
+#HTML tag removal function
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
+#Taking URL as input
 URL = input("Enter the URL here: ")
 
+#Passing URL to requests, then BeautifulSoup, left with title as list.
 res = requests.get(URL.strip())
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 title_list = soup.select('title')
 
+#Pulling title from list
 title = title_list[0]
 
+#Print statement
 print("The title of the web page at " + URL + "is: \"" + remove_html_tags(str(title)) + "\"")
 
